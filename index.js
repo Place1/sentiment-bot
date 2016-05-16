000d9618af484e11d09355f5d5057a7560b60dad
James Batt <place.net.au@gmail.com>
2016-05-16 23:37:26 +1000

    updated env setting name

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ require('coffee-script/register'); // so we can require hubot;
 const Hubot = require('hubot');
 const path = require('path');
 
-const adapterPath = undefined;
+const adapterPath = path.resolve(__dirname);
 const adapterName = 'facebook'; // from hubot-facebook
 const enableHttpd = true;
 const botName = 'sentiment-bot';
@@ -13,6 +13,7 @@ const privateSettings = require('./privateSettings');
 
 process.env.FB_LOGIN_EMAIL = privateSettings.FB_LOGIN_EMAIL;
 process.env.FB_LOGIN_PASSWORD = privateSettings.FB_LOGIN_PASSWORD;
+process.env.FB_SELF_LISTEN = true;
 
 // create the bot with the above settigns
 const robot = new Hubot.Robot(adapterPath, adapterName, enableHttpd, botName, botAlias);
